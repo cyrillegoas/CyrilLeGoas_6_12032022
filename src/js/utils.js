a7a8f190186e6f389e3808e2c98ccc28f4db2a3a
Cyril Le Goas <cyril.legoas@gmail.com>
2022-03-25 23:49:57 +0100

    ADD wait function

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,3 +3,8 @@ export async function fetchJson(path) {
   const data = await res.json();
   return data;
 }
+
+/* eslint-disable */
+export const wait = (amount = 0) =>
+  new Promise((resolve) => setTimeout(resolve, amount));
+/* eslint-enable */
