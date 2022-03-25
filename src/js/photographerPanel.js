1c8038877324060f52450e448e1f9f60708517a8
Cyril Le Goas <cyril.legoas@gmail.com>
2022-03-25 23:53:36 +0100

    ADD contact modal open/close/keyboard control/

--- src/js/photographerPanel.js
+++ src/js/photographerPanel.js
@@ -1,3 +1,5 @@
+import { wait } from './utils';
+
 /**
  * Generate photographer info panel html.
  * @param {object} photographerInfo - photographer info for the current page.
@@ -29,6 +31,75 @@ function generateInfoPanel(photographerInfo) {
 }
 
 /**
+ * Constructor function for contact modal
+ * @param {object} modal - modal's container element.
+ * @param {object} photographerInfo - photographer info for the current page
+ */
+function ContactModal(modal, photographerInfo) {
+  if (!(modal instanceof Element)) {
+    throw new Error('No modal found!');
+  }
+  this.modal = modal;
+  const modalTitle = modal.querySelector('.contact-modal__title');
+  this.closeModalButton = modal.querySelector('button.contact-modal__close');
+  this.submitButton = modal.querySelector('button[aria-label="send message"]');
+  const from = modal.querySelector('form.contact-modal__form');
+
+  modalTitle.innerHTML = `Contactez-moi<br />${photographerInfo.name}`;
+
+  // Event listener
+  modal.addEventListener('click', (event) => this.handleClickOutside(event));
+  this.closeModalButton.addEventListener('click', () => this.closeModal());
+  from.addEventListener('submit', (event) => this.submitForm(event));
+  modal.addEventListener('keydown', (event) => this.handleKeyDown(event));
+}
+
+ContactModal.prototype.openModal = async function () {
+  this.modal.removeAttribute('hidden');
+  await wait();
+  this.modal.setAttribute('aria-hidden', 'false');
+};
+
+ContactModal.prototype.closeModal = async function () {
+  this.modal.setAttribute('aria-hidden', 'true');
+  await wait(500);
+  this.modal.setAttribute('hidden', '');
+};
+
+ContactModal.prototype.handleClickOutside = function (event) {
+  if (event.currentTarget === event.target) this.closeModal();
+};
+
+ContactModal.prototype.handleKeyDown = function (event) {
+  if (
+    event.key === 'Tab' &&
+    event.shiftKey &&
+    document.activeElement === this.closeModalButton
+  ) {
+    event.preventDefault();
+    this.submitButton.focus();
+  } else if (
+    event.key === 'Tab' &&
+    !event.shiftKey &&
+    document.activeElement === this.submitButton
+  ) {
+    event.preventDefault();
+    this.closeModalButton.focus();
+  }
+};
+
+ContactModal.prototype.submitForm = function (event) {
+  event.preventDefault();
+  const inputs = this.modal.querySelectorAll('input');
+  const message = this.modal.querySelector('.contact-modal__message');
+  Array.from(inputs).forEach((input) => {
+    console.log(`${input.name}: ${input.value}`);
+  });
+  console.log(`${message.name}: ${message.value}`);
+  this.closeModal();
+};
+
+/**
  * Fill the photographer information panel.
  * @param {object} infoSection - photographer info section element.
  * @param {object} photographerInfo - photographer info for the current page.
@@ -36,4 +107,12 @@ function generateInfoPanel(photographerInfo) {
 export default function infoPanelInit(infoSection, photographerInfo) {
   const infoPanelHTML = generateInfoPanel(photographerInfo);
   infoSection.insertAdjacentHTML('afterbegin', infoPanelHTML);
+  const openModalButton = document.querySelector(
+    'button[aria-label="contact me"]'
+  );
+  const modal = new ContactModal(
+    document.querySelector('.contact-modal'),
+    photographerInfo
+  );
+  openModalButton.addEventListener('click', () => modal.openModal());
 }
